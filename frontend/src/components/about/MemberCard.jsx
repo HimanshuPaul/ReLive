4105b4e6e22a54ee5ccbf6bd2b01bed974cdaecd
nikitaMishr <nikitamishra9743@gmail.com>
2025-09-08 15:51:54 +0530

    made changes in about us page

--- frontend/src/components/about/MemberCard.jsx
+++ frontend/src/components/about/MemberCard.jsx
@@ -8,8 +8,8 @@ export default function MemberCard({ name, role, img }) {
         alt={name}
         className="w-40 h-40 object-cover rounded-full border-4 border-purple-600 mb-4"
       />
-      <h3 className="text-2xl font-bold text-gray-900 text-center">{name}</h3>
-      <p className="text-lg text-gray-600 text-center">{role}</p>
+      <h3 className="text-2xl font-bold text-white text-center">{name}</h3>
+      <p className="text-lg text-white text-center">{role}</p>
     </div>
   );
 }
